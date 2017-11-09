4aa0327c8fe31e708f96765c0597a72ef93b9d64
Robert Shaw <r.shaw1@yahoo.com>
2017-11-09 15:28:20 -0600

    created portable db

---
+++ server/db/index.js
@@ -0,0 +1,31 @@
+'use strict'
+const chalk = require('chalk');
+
+// run our models file (makes all associations for our Sequelize objects)
+require('./models')
+const db = (require('./db'))
+
+// sync the db, creating it if necessary
+function sync(force=false, retries=0, maxRetries=5) {
+  return db.sync({force})
+  .then(ok => console.log(`Synced models to db ${connectionString}`))
+  .catch(fail => {
+    // Don't do this auto-create nonsense in prod, or
+    // if we've retried too many times. 
+    if (process.env.NODE_ENV === 'production' || retries > maxRetries) {
+      console.error(chalk.red(`********** database error ***********`))
+      console.error(chalk.red(`    Couldn't connect to ${connectionString}`))
+      console.error()
+      console.error(chalk.red(fail))
+      console.error(chalk.red(`*************************************`))
+      return
+    }
+    // Otherwise, do this autocreate nonsense
+    console.log(`${retries ? `[retry ${retries}]` : ''} Creating database ${name}...`)
+    return new Promise((resolve, reject) =>
+      require('child_process').exec(`createdb "${name}"`, resolve)
+    ).then(() => sync(true, retries + 1))
+  })
+}
+
+db.didSync = sync();
