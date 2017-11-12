6065132fac15d72148338be45ed92b9c63df2309
Robert Shaw <r.shaw1@yahoo.com>
2017-11-11 19:28:21 -0600

    added logging statement and updated const

--- server/db/db.js
+++ server/db/db.js
@@ -1,16 +1,16 @@
 'use strict'
-// const debug = require('debug')('sql');
+const debug = require('debug')('sql');
 const chalk = require('chalk');
 const Sequelize = require('sequelize');
-const pkg = require('../../package.json');
+const dbName = require('../../package.json').name;
 
-const name = process.env.DATABASE_NAME || pkg.name;
+const name = process.env.DATABASE_NAME || dbName;
 const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${name}`;
 
 console.log(chalk.yellow(`Opening database connection to ${connectionString}`));
 
 // create the database instance that can be used in other database files
 const db = module.exports = new Sequelize(connectionString, {
-  // logging: debug, // export DEBUG=sql in the environment to get SQL queries 
+  logging: debug, // export DEBUG=sql in the environment to get SQL queries 
   native: true    // lets Sequelize know we can use pg-native for ~30% more speed (this may cause problems so take it out if necessary)
 });
