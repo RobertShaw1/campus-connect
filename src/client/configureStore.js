ab4eebeb0ca061a7bf0021ff67af9c060607717b
Robert Shaw <r.shaw1@yahoo.com>
2017-11-29 21:06:57 -0600

    initial configuration for redux store

---
+++ src/client/configureStore.js
@@ -0,0 +1,27 @@
+// NODE Modules
+import {createStore, applyMiddleware, combineReducers, compose} from 'redux'
+import {connectRoutes} from 'redux-first-router'
+
+// LOCAL Modules
+import routesMap from './routesMap'
+
+import * as reducers from './reducers'
+import * as actionCreators from './actions'
+
+export default history => {
+  const { 
+    reducer,
+    middleware,
+    enhancer
+  } = connectRoutes(history, routesMap)
+
+  const rootReducer = combineReducers({ ...reducers, location: reducer })
+  const middlewares = applyMiddleware(middleware)
+  const enhancers = composeEnhancers(enhancer, middlewares)
+
+  return createStore(rootReducer, enhancers)
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ actionCreators })
+  : compose
