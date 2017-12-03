7f1fc768319ef547993a936ecc5cfab1cae58277
Robert Shaw <r.shaw1@yahoo.com>
2017-12-03 17:08:06 -0600

    setup server to use webpack-dev-middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,19 @@
 'use strict'
 const path = require('path');
 const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
 const app = express();
+const config = require('../../webpack.config.js');
+const compiler = webpack(config);
+
+/*
+ Tell express to use the webpack-dev-middleware and use the webpack.config.js file as a base.
+ */
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath
+}));
 
 const PUBLIC_DIR = path.resolve(__dirname, '../../public');
 const PORT = 3000;
