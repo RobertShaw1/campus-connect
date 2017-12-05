d9f30888b2fa29932cd21d76062fbe527a7109b5
Robert Shaw <r.shaw1@yahoo.com>
2017-12-05 00:27:30 -0600

    storybook is fully connected

--- src/client/stories/index.js
+++ src/client/stories/index.js
@@ -5,9 +5,16 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import Home from '../components/Home';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+import '../../../public/index.css'
+
+storiesOf('Welcome', module)
+  .add('to Storybook', () => <Welcome showApp={linkTo('Button')} />)
+  .add('Home', () => <Home />)
+
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>)
+  .add('with nothing', () => <Button />)
