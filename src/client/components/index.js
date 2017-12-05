65fb821d7b6b1c18d6cb889a4ee8c4426fa4645f
Robert Shaw <r.shaw1@yahoo.com>
2017-12-05 11:32:28 -0600

    created a single source export for all container components

---
+++ src/client/components/index.js
@@ -0,0 +1,11 @@
+/**
+ * `components/index.js` exists simply as a 'central export' for our components.
+ * This way, we can import all of our components from the same place, rather than
+ * having to figure out which file they belong to!
+ */
+
+export {default as Home} from './views/Home';
+export {default as AllCampuses} from './views/AllCampuses';
+export {default as AllStudents} from './views/AllStudents';
+export {default as SingleCampus} from './views/SingleCampus';
+export {default as SingleStudent} from './views/SingleStudent';
