7c23af6b3c64a371c4bbf42f5ede460fab992e2d
Robert Shaw <r.shaw1@yahoo.com>
2017-12-05 19:00:28 -0600

    updated webpack config for storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,18 @@
-
+const path = require('path');
 const config = require('../webpack.config.js')(process.env);
 
 module.exports = (config) => {
   // Extend it as you need.
 
   // For example, add css and style loaders:
-  config.module.rules.push({
-    test: /\.css$/,
-    use: ['style-loader', 'css-loader'],
-  });
+  config.module.rules.push(
+    {
+      test: /\.css$/,
+      include: path.resolve(__dirname, './storybook.css'),
+      exclude: /(node_modules|bower_components)/,
+      use: ['style-loader', 'css-loader'],
+    },
+  );
   config.resolve.extensions.push('.css');
 
   return config;
