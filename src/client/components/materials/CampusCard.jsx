4f511f4e16f49df3e646336533b00cd7d0b09096
Robert Shaw <r.shaw1@yahoo.com>
2017-12-07 00:22:33 -0600

    created CampusCard

---
+++ src/client/components/materials/CampusCard.jsx
@@ -0,0 +1,51 @@
+// @flow weak
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import Modal from './Modal';
+
+const styles = {
+  card: {
+    maxWidth: 445,
+  },
+  media: {
+    height: 500,
+  },
+};
+
+function SimpleMediaCard(props) {
+  const { classes } = props;
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image="http://graphql.org/img/logo.svg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography type="headline" component="h2">
+            Lizard
+          </Typography>
+          <Typography component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Modal text={'View Campus'} />
+        </CardActions>
+      </Card>
+    </div>
+  );
+}
+
+SimpleMediaCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleMediaCard);
