77b96e777c20e531509c31d49e3f028f4b2357cd
Robert Shaw <r.shaw1@yahoo.com>
2017-12-07 00:26:33 -0600

    index file for materials

---
+++ src/client/components/materials/index.js
@@ -0,0 +1,11 @@
+/**
+ * `components/index.js` exists simply as a 'central export' for our components.
+ * This way, we can import all of our components from the same place, rather than
+ * having to figure out which file they belong to!
+ */
+
+
+export {default as NavBar} from './NavBar';
+export {default as CampusCard} from './CampusCard';
+export {default as StudentCard} from './StudentCard';
+
