71bf59a5688e91c59da0739e10f5c3c5f0d9d000
Robert Shaw <r.shaw1@yahoo.com>
2017-12-07 00:21:27 -0600

    created basic Home/Landing Page

--- src/client/components/Views/Home.jsx
+++ src/client/components/Views/Home.jsx
@@ -2,12 +2,25 @@
 
 import React from 'react';
 
+const styles = {
+  main: {
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: `url(https://images.pexels.com/photos/247819/pexels-photo-247819.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb) no-repeat`,
+    // background: `url(https://images.pexels.com/photos/126271/pexels-photo-126271.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb) no-repeat`,
+    backgroundSize: 'cover',
+  },
+  image: {
+    background: 'cover',
+  }
+}
+
 const Home = () => {
   return(
-    <div>
-      Well Hello There!
-      You look handsome ;-D
-    </div>
+  <div style={styles.main}>
+  </div>
   )
 }
 
