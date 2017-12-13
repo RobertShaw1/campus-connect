5984bd2e77780964109e30852a11b98ded496bf7
Robert Shaw <r.shaw1@yahoo.com>
2017-12-12 19:59:48 -0600

    added thunk to CAMPUSES and imported babel-poyfill to enable use of async await

--- src/client/routesMap.js
+++ src/client/routesMap.js
@@ -1,8 +1,17 @@
+import 'babel-polyfill'
 import { redirect, NOT_FOUND } from 'redux-first-router'
+import {client, allCampusesQuery} from './graphql-client'
 
 const routesMap = {
   HOME: '/',
-  CAMPUSES: '/campuses',
+  CAMPUSES: {
+    path: '/campuses',
+    thunk: async (dispatch, getState) => {
+      const campuses = await client.request(allCampusesQuery);
+      const action = {type: 'CAMPUSES_FETCHED', payload: campuses}
+      return dispatch(action)
+    }
+  },
   STUDENTS: '/students',
   SINGLECAMPUS: '/campuses/:id',
   SINGLESTUDENT: '/students/:id',
