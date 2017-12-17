8bfcecfc63918503325d70f6939fa4aff626bce3
Robert Shaw <r.shaw1@yahoo.com>
2017-12-16 22:35:35 -0600

    refactored seed file

--- seed.js
+++ seed.js
@@ -1,19 +1,21 @@
 // NODE Modules
 const Promise = require('bluebird');
+const chance = require('chance')();
 
 // LOCAL Modules
 const db = require('./src/server/db/db');
 const {Campus, Student} = require('./src/server/db/models');
+const students = require('./students');
 
 const campuses = [
   {
     name: 'GraphQL',
-    imgURL: 'http://graphql.org/',
+    imgURL: 'http://graphql.org/img/logo.svg',
     description: 'The Best Query Language',
   },
   {
     name: 'Apollo',
-    imgURL: 'http://dev.apollodata.com',
+    imgURL: 'http://www.discovermeteor.com/images/blog/apollo-logo.png',
     description: 'Awesome GraphQL Client',
   },
   {
@@ -25,74 +27,7 @@ const campuses = [
 
 const createRandomCampusId = () => Math.ceil(Math.random() * (campuses.length));
 
-const students = [{
-  name: 'Cody',
-  email: 'cody@cody.com',
-}, {
-  name: 'Ben',
-  email: 'ben@ben.com',
-}, {
-  name: 'Star',
-  email: 'star@star.com',
-}, {
-  name: 'Batman',
-  email: 'batman@batman.com',
-}, {
-  name: 'Elliott',
-  email: 'elliot@elliot.com',
-}, {
-  name: 'Fira',
-  email: 'fira@fira.com',
-}, {
-  name: 'Henry',
-  email: 'henry@henry.com',
-}, {
-  name: 'Marcy',
-  email: 'marcy@marcy.com',
-}, {
-  name: 'Milton',
-  email: 'milton@milton.com',
-}, {
-  name: 'Murphy',
-  email: 'murphy@murphy.com',
-}, {
-  name: 'Raffi',
-  email: 'raffi@raffi.com',
-}, {
-  name: 'Tulsi',
-  email: 'tulsi@tulsi.com',
-}, {
-  name: 'Pork Chop',
-  email: 'porkchop@porkchop.com',
-}, {
-  name: 'Ribs',
-  email: 'ribs@ribs.com',
-}, {
-  name: 'Stacey',
-  email: 'stacey@stacey.com',
-}, {
-  name: 'JD',
-  email: 'jd@jd.com',
-}, {
-  name: 'Slim',
-  email: 'slim@slim.com',
-}, {
-  name: 'Yoda',
-  email: 'yoda@yoda.com',
-}, {
-  name: 'Mark',
-  email: 'mark@mark.com',
-}, {
-  name: 'Larry',
-  email: 'larry@larry.com',
-}, {
-  name: 'Lisa',
-  email: 'lisa@lisa.com',
-}, {
-  name: 'Odie',
-  email: 'odie@odie.com',
-}];
-
+students.forEach(student => student.phone = chance.phone());
 
 const seed = () => (
   Promise.all(campuses.map(campus => {
@@ -101,7 +36,7 @@ const seed = () => (
   }))
   .then(() =>
     Promise.all(students.map(student => {
-      console.log(student)      
+      console.log(student)
       return (
         Student.create(student)
         .then(student => {
