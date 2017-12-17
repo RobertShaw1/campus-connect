732d6263c88a80e62d4e9319c361b1be701da373
Robert Shaw <r.shaw1@yahoo.com>
2017-12-17 06:28:51 -0600

    connected AllStudents component to Apollo

--- src/client/components/Views/AllStudents.jsx
+++ src/client/components/Views/AllStudents.jsx
@@ -1,6 +1,9 @@
 'use strict';
 
 import React from 'react';
+import {compose, graphql, withApollo} from 'react-apollo';
+import gql from 'graphql-tag';
+
 import {StudentCard} from '../materials';
 
 const styles = {
@@ -11,18 +14,44 @@ const styles = {
   flexWrap: 'wrap',
 }
 
-const AllStudents = () => {
-  return (
-    <div style={styles}>
-      <StudentCard />
-      <StudentCard />
-      <StudentCard />
-      <StudentCard />
-      <StudentCard />
-      <StudentCard />
-      <StudentCard />
-    </div>
-  )
+const AllStudents = ({AllStudentsQuery}) => {
+  if(AllStudentsQuery.loading) {
+    return <div>Loading...</div>
+  } else {
+    const {allStudents} = AllStudentsQuery;
+    return (
+      <div style={styles}>
+      {
+        allStudents.map(student => {
+          // console.log('student.campus.name = ', student.campuse.name)
+          return (<StudentCard
+            key={student.id}
+            name={student.name}
+            // campus={student.campus.name}
+            imgURL={student.imgURL}
+          />)
+        })
+      }
+      </div>
+    )
+  }
 }
 
-export default AllStudents;
+const ALL_STUDENTS_QUERY = gql`
+  query AllStudentsQuery {
+    allStudents {
+      id
+      name
+      imgURL
+      campus {
+        name
+      }
+    }
+  }
+`
+
+export default compose(
+  withApollo,
+  graphql(ALL_STUDENTS_QUERY, {name: 'AllStudentsQuery'}),
+  // connect()
+)(AllStudents);
