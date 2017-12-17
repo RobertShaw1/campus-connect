e4e6d74949bc5c8fe66faa496f4427a50deb89b7
Robert Shaw <r.shaw1@yahoo.com>
2017-12-17 06:26:33 -0600

    updated default img for Student Model

--- src/server/db/models/student.js
+++ src/server/db/models/student.js
@@ -21,7 +21,7 @@ const Student = db.define('student', {
   },
   imgURL: {
     type: Sequelize.STRING,
-    defaultValue: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+    defaultValue: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCmPyKqPknUxCiBrXNUYxAA89HQWh5UrxvyKnSjRggq-mUZFLe'
   },
 })
 
